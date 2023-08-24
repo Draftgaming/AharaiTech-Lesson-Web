a9b7307b067156c558be39f4f2834a729bab7119
Draftgaming <130181218+Draftgaming@users.noreply.github.com>
2023-08-24 03:09:53 +0300

    fixed log in just need to add more home styling

--- Sign-in/Sign_in.js
+++ Sign-in/Sign_in.js
@@ -1,17 +1,33 @@
-window.onload = function() {
-    const codeInput = document.getElementById('code');
-    const passwordInput = document.getElementById('password');
-    const continueButton = document.getElementById('continue-btn');
-    
-    // Function to enable the Continue button when both fields are valid
-    function validateInputs() {
-        const isCodeValid = codeInput.value.length === 9;
-        const isPasswordValid = passwordInput.value.length >= 6; // Example password length validation
-        
-        continueButton.disabled = !(isCodeValid && isPasswordValid);
+
+const formOpenBtn = document.querySelector("#form-open"),
+  home = document.querySelector(".home"),
+  formContainer = document.querySelector(".form_container"),
+  formCloseBtn = document.querySelector(".form_close"),
+  signupBtn = document.querySelector("#signup"),
+  loginBtn = document.querySelector("#login"),
+  pwShowHide = document.querySelectorAll(".pw_hide");
+
+formOpenBtn.addEventListener("click", () => home.classList.add("show"));
+formCloseBtn.addEventListener("click", () => home.classList.remove("show"));
+
+pwShowHide.forEach((icon) => {
+  icon.addEventListener("click", () => {
+    let getPwInput = icon.parentElement.querySelector("input");
+    if (getPwInput.type === "password") {
+      getPwInput.type = "text";
+      icon.classList.replace("uil-eye-slash", "uil-eye");
+    } else {
+      getPwInput.type = "password";
+      icon.classList.replace("uil-eye", "uil-eye-slash");
     }
-    
-    // Add event listeners to inputs
-    codeInput.addEventListener('input', validateInputs);
-    passwordInput.addEventListener('input', validateInputs);
-}
+  });
+});
+
+signupBtn.addEventListener("click", (e) => {
+  e.preventDefault();
+  formContainer.classList.add("active");
+});
+loginBtn.addEventListener("click", (e) => {
+  e.preventDefault();
+  formContainer.classList.remove("active");
+});
